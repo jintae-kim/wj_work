c45bc6a763627f60ec8ff2fcf744f213792fd9c3
woojae <ds6318@naver.com>
2023-09-21 15:15:22 +0900

    feat:splitter 적용

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -3,18 +3,21 @@ import { Gnb, Aside, HistoryStack } from "./components/Include";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Outlet } from "react-router-dom";
 import { Container, Row, Col } from "react-bootstrap";
+import { Split } from "@geoffcox/react-splitter";
 
 const LayoutProvider = () => {
   return (
     <Container fluid className="container-wrapper">
       <Gnb />
       <Row className="contents-wrapper">
-        <Col className="aside-section">
-          <Aside />
-        </Col>
-        <Col xs="10" className="contents-section">
-          <Outlet />
-        </Col>
+        <Split initialPrimarySize='300px' minPrimarySize='10px' minSecondarySize='calc(100% - 300px)' splitterSize='5px' vertical>
+          <div className="aside-section">
+            <Aside />
+          </div>
+          <div className="contents-section">
+            <Outlet />
+          </div>
+        </Split>
       </Row>
       <HistoryStack />
     </Container>
