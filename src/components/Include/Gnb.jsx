68d345d898dcd3f0f6fe763d96ee6af2836e7f96
woojae <ds6318@naver.com>
2023-09-22 12:12:33 +0900

    feat:devextreme 적용

--- src/components/Include/Gnb.jsx
+++ src/components/Include/Gnb.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Nav } from "react-bootstrap";
 import { NavLink } from "react-router-dom"
 import "../../assets/gnb.css"
 
--- src/components/Include/HistoryStack.jsx
+++ src/components/Include/HistoryStack.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Stack } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "../../assets/history.css"
 
